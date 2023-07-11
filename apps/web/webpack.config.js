7430957b265a23368073b626221d6d8fcc085139
Shane Melton <smelton@bitwarden.com>
2023-07-11 14:04:22 -0700

    Update webpack config to support proxying web socket connections for notifications

--- apps/web/webpack.config.js
+++ apps/web/webpack.config.js
@@ -213,6 +213,7 @@ const devServer =
             pathRewrite: { "^/notifications": "" },
             secure: false,
             changeOrigin: true,
+            ws: true,
           },
           "/icons": {
             target: envConfig.dev?.proxyIcons,
